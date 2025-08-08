969bb016299d2a0beac05b38f7ffa1cc9c5d78b2
Delta <48131223+TheGamer1002@users.noreply.github.com>
2025-08-07 23:54:49 -0400

    Add 2027 alphas

--- Casks/e/elastic-dashboard@2027.rb
+++ Casks/e/elastic-dashboard@2027.rb
@@ -1,12 +1,17 @@
-cask "elastic-dashboard" do
-  version "2025.2.2"
-  sha256 "e6ae12f79b5bbfd6062d7b5172549613cdb2c55202a342fd44074a3037f3c654"
+cask "elastic-dashboard@2027" do
+  version "2027.0.0-alpha3"
+  sha256 "12a7f62c4e25ac5264fbb3e920d56482b70c2403e08c2020d35dd61909590681"
 
   url "https://github.com/Gold872/elastic-dashboard/releases/download/v#{version}/elastic-setup-macos.dmg",
       verified: "github.com/Gold872/elastic-dashboard/"
   name "elastic-dashboard"
-  desc "Simple and modern dashboard for FRC"
+  desc "Simple and modern dashboard for FRC (2027 SystemCore alpha)"
   homepage "https://frc-elastic.gitbook.io/docs"
+
+  livecheck do
+    url "https://github.com/Gold872/elastic-dashboard.git"
+    regex(/^v?(2027[a-zA-Z0-9\-\.]*)$/i)
+  end
 
   depends_on macos: ">= :mojave"
 
