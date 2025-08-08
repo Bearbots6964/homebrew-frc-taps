4b0e6ad658c73ef85b63c4b851567bddc1449692
Delta <48131223+TheGamer1002@users.noreply.github.com>
2025-08-07 21:21:32 -0400

    Fix style

--- Casks/e/elastic-dashboard.rb
+++ Casks/e/elastic-dashboard.rb
@@ -3,9 +3,9 @@ cask "elastic-dashboard" do
   sha256 "e6ae12f79b5bbfd6062d7b5172549613cdb2c55202a342fd44074a3037f3c654"
 
   url "https://github.com/Gold872/elastic-dashboard/releases/download/v#{version}/elastic-setup-macos.dmg",
-    verified: "github.com/Gold872/elastic-dashboard"
+      verified: "github.com/Gold872/elastic-dashboard/"
   name "elastic-dashboard"
-  desc "A simple and modern dashboard for FRC"
+  desc "Simple and modern dashboard for FRC"
   homepage "https://frc-elastic.gitbook.io/docs"
 
   depends_on macos: ">= :mojave"
