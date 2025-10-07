8aba14f813efcd333a5d1f56e25fee703e58272d
Delta <48131223+TheGamer1002@users.noreply.github.com>
2025-10-07 08:54:40 -0400

    update releases, add new 2026 release

--- Casks/e/elastic-dashboard@2026.rb
+++ Casks/e/elastic-dashboard@2026.rb
@@ -1,16 +1,16 @@
-cask "elastic-dashboard@2027" do
-  version "2027.0.0-alpha4"
-  sha256 "48f0a7127318f3b0e9fa557e1a3d9cb6ae0b23000b64a1864d669ee89bc0367d"
+cask "elastic-dashboard@2026" do
+  version "2026.0.0-beta-1"
+  sha256 "7077a3f406e5d68762721aead2615b8b372370822639056f703748fee69cab23"
 
   url "https://github.com/Gold872/elastic-dashboard/releases/download/v#{version}/elastic-setup-macos.dmg",
       verified: "github.com/Gold872/elastic-dashboard/"
   name "elastic-dashboard"
-  desc "Simple and modern dashboard for FRC (2027 SystemCore alpha)"
+  desc "Simple and modern dashboard for FRC (2026 beta)"
   homepage "https://frc-elastic.gitbook.io/docs"
 
   livecheck do
     url "https://github.com/Gold872/elastic-dashboard.git"
-    regex(/^v?(2027[a-zA-Z0-9\-\.]*)$/i)
+    regex(/^v?(2026[a-zA-Z0-9\-\.]*)$/i)
   end
 
   depends_on macos: ">= :mojave"
