eb12bb9525da1e2cc3d01bcd1dddd7b7da627c6d
Delta <48131223+TheGamer1002@users.noreply.github.com>
2026-01-14 20:27:08 -0500

    elastic-dashboard 2026.1.0

--- Casks/e/elastic-dashboard.rb
+++ Casks/e/elastic-dashboard.rb
@@ -1,12 +1,17 @@
 cask "elastic-dashboard" do
-  version "2025.2.2"
-  sha256 "e6ae12f79b5bbfd6062d7b5172549613cdb2c55202a342fd44074a3037f3c654"
+  version "2026.1.0"
+  sha256 "ec5e6f511a1b0cc15d50e44199406529528bc7a3a59c18e52828b27b44cd6de7"
 
   url "https://github.com/Gold872/elastic-dashboard/releases/download/v#{version}/elastic-setup-macos.dmg",
       verified: "github.com/Gold872/elastic-dashboard/"
   name "elastic-dashboard"
-  desc "Simple and modern dashboard for FRC"
+  desc "Simple and modern dashboard for FRC (2026 beta)"
   homepage "https://frc-elastic.gitbook.io/docs"
+
+  livecheck do
+    url "https://github.com/Gold872/elastic-dashboard.git"
+    regex(/^v?(2026[a-zA-Z0-9\-.]*)$/i)
+  end
 
   depends_on macos: ">= :catalina"
 
